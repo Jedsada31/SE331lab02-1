1683d6a580f78cad882b54106ffc4ae5be1e0372
Jedsada Kampen <Jedsada.kampen@cmu.ac.th>
2022-07-16 23:56:16 +0700

    Add product-details

---
+++ components/Product-details.js
@@ -0,0 +1,13 @@
+app.component("product-details", {
+    props: {
+        details: {
+            type: Array,
+            required: true,
+        },
+    },
+    template:
+        /*html*/
+        `<ul v-for="(detail, index) in details" :key="index" class="product-details">
+            <li>{{ detail }}</li>
+        </ul>`,
+});
